89b0f77b86225b0b9ad42799d5dc207bceae9038
Matthew Wilding <mbwilding@gmail.com>
2025-05-10 21:01:57 +0800

    2025-05-10-21-01-57

--- crates/sau_worker/build.rs
+++ crates/sau_worker/build.rs
@@ -4,14 +4,12 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let pattern = "../../target/*/build/steamworks-sys-*/out/*";
-    let files: Vec<PathBuf> = glob(pattern)?
+    // let pattern = "../../target/*/build/steamworks-sys-*/out/*";
+    let pattern = "../../**/target/*/build/steamworks-sys-*/out/*";
+    let src_path: PathBuf = glob(pattern)?
         .filter_map(Result::ok)
-        .collect();
-
-    if files.is_empty() {
-        return Err("No steam_api files found".into());
-    }
+        .next()
+        .ok_or("steam_api file not found")?;
 
     let out_dir = env::var("OUT_DIR")?;
     let out_path = Path::new(&out_dir);
@@ -21,11 +19,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .and_then(|p| p.parent())
         .ok_or("Failed to locate target directory")?;
 
-    for file in files {
-        let file_name = file.file_name().ok_or("No file name")?;
-        let dest_path = target_dir.join(file_name);
-        fs::copy(&file, &dest_path)?;
-    }
+    let file_name = src_path.file_name().ok_or("No file name")?;
+    let dest_path = target_dir.join(file_name);
+
+    fs::copy(&src_path, &dest_path)?;
 
     Ok(())
 }
